e8bc201ff50a8786aa4000d84986d51a2ccfd4f1
mukamisha <mukamishajacky97@gmail.com>
2019-10-21 16:21:01 +0200

    adding profile edit

--- insta_app/views.py
+++ insta_app/views.py
@@ -33,3 +33,17 @@ def profile(request, username=None):
     current_user = request.user
     pictures = Image.objects.filter(user=current_user)
     return render(request,"profile.html",locals(),{"pictures":pictures})
+
+@login_required(login_url='/accounts/login/')
+def profile_edit(request):
+   current_user=request.user
+   if request.method=='POST':
+       form=ProfileForm(request.POST,request.FILES)
+       if form.is_valid():
+           image=form.save(commit=False)
+           image.user=current_user
+           image.save()
+       return redirect('profile')
+   else:
+       form=ProfileForm()
+   return render(request,'profile_edit.html',{"form":form})
